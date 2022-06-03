030f1fa4c9695a99bdf498913cbaa9836c5fb9f0
Bruksin <ats@sl.ru>
2022-06-03 20:01:32 +0300

    урок 2

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Хеллоу ворлд");
+        byte a = 127;
+        System.out.println(a);
+        System.out.println(a + 1);
     }
 }
