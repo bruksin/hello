11b502acbb6b6213ac3e55fafd495262f428b49a
Bruksin <ats@sl.ru>
2022-06-06 22:09:11 +0300

    Лабораторная работа_2

---
+++ src/Laba2.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Laba2 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int secret = 10;
+        int popytka;
+        System.out.println("Угадай число");
+        do {
+            System.out.print("Введите число от 1 до 100: ");
+            popytka = scan.nextInt();
+            if (popytka != secret) {
+                System.out.println("Не угадал, Попробуй еще раз!");
+            }
+        } while (popytka != secret);
+        System.out.println("Угадал!!!");
+    }
+}
