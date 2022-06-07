201d05c217fbdbbe9b03792861c2af896252eba0
bruksin <ats@sl.ru>
2022-06-07 16:01:12 +0300

    урок 5 лаба 1

--- src/Laba3.java
+++ src/Laba3.java
@@ -4,7 +4,8 @@ public class Laba3 {
         int[] a = {2, 3, 4, 5, 6, 7, 8, 9, 10};
         int proizvedenie = 1;
         for (int i=0;i<9;i++) {
-            proizvedenie *= a[i];
+            if (i==0) proizvedenie = a[i];
+            else proizvedenie *= a[i];
         }
 
         System.out.println("Произведение = " + proizvedenie);
