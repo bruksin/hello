e5a86918747fa8ad23f04baec00000299896e612
bruksin <ats@sl.ru>
2022-06-07 15:56:15 +0300

    урок 5 лаба 2

---
+++ src/Laba4.java
@@ -0,0 +1,11 @@
+//Лабораторная работа 2_Массивы_Циклы Смирнов АН
+
+public class Laba4 {
+    public static void main(String[] args) {
+        for (int i=1; i<=7;i++) {
+            for (int j=1; j<=7;j++) {
+                System.out.println(i + "*" + j + "=" + i*j);
+            }
+        }
+    }
+}
