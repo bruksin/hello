3a46cd3106ff39f89c2491f5c8e4a8e36430205e
bruksin <ats@sl.ru>
2022-06-23 15:14:06 +0300

    //Лабораторная работа 4_Массивы_Циклы

--- src/Laba4.java
+++ src/Laba4.java
@@ -3,7 +3,7 @@
 public class Laba4 {
     public static void main(String[] args) {
         for (int i=1; i<=7;i++) {
-            for (int j=1; j<=7;j++) {
+            for (int j=1; j<=10;j++) {
                 System.out.println(i + "*" + j + "=" + i*j);
             }
         }
